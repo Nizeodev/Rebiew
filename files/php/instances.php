f5956bb048a7d02459d81ab3b7c110876384a441
Nizeodev <117823035+Nizeodev@users.noreply.github.com>
2025-06-13 02:36:44 +0400

    New

--- files/php/instances.php
+++ files/php/instances.php
@@ -1,7 +1,7 @@
 <?php
-$instance['hypixel'] = array_merge($instance['hypixel'], array(
+$instance['rebiew'] = array_merge($instance['rebiew'], array(
     "loadder" => array(
-        "minecraft_version" => "1.8.9",
+        "minecraft_version" => "1.20.1",
         "loadder_type" => "forge",
         "loadder_version" => "latest"
     ),
@@ -21,26 +21,8 @@ $instance['hypixel'] = array_merge($instance['hypixel'], array(
     "whitelist" => array(),
     "whitelistActive" => false,
     "status" => array(
-        "nameServer" => "Hypixel",
-        "ip" => "mc.hypixel.net",
-        "port" => 25565
+        "nameServer" => "Rebiew",
+        "ip" => "panel.code-master.xyz",
+        "port" => 5105
     )
 ));
-
-$instance['PokeMoonX'] = array_merge($instance['PokeMoonX'], array(
-    "loadder" => array(
-        "minecraft_version" => "1.16.5",
-        "loadder_type" => "forge",
-        "loadder_version" => "1.16.5-36.2.35"
-    ),
-    "verify" => false,
-    "ignored" => array(),
-    "whitelist" => array(),
-    "whitelistActive" => false,
-    "status" => array(
-        "nameServer" => "PokeMoonX",
-        "ip" => "178.32.106.234",
-        "port" => 25599
-    )
-));
-?>
